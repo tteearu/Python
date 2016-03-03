70ff808351f21d724ca62be1d83777b846c7db3a
Taavi <tteearu@itcollege.ee>
2016-03-03 20:18:56 +0200

    Statistics
    Added statistics about:
    bandwidth hoggers
    countries
    visited url-s
    visited usernames
    traffic by devices

--- Praks2/snoop.py
+++ Praks2/snoop.py
@@ -1,17 +1,28 @@
 import os
- 
-# Following is the directory with log files,
-# On Windows substitute it where you downloaded the files
-root = "/home/tteearu/logs"
+import argparse
+import gzip
+import urllib
+import GeoIP
+
+parser = argparse.ArgumentParser(description='Apache2 log parser')
+parser.add_argument("--path", help="Path to Apache2 log files", default="/var/log/apache2")
+parser.add_argument("--top-urls", help="Find top URL-s", default="store_true")
+parser.add_argument("--geoip", help="Resolve IP-s to country codes", default="store_true")
+parser.add_argument("--verbose", help="Increase verbosity", default="store_true")
+args = parser.parse_args()
+
+root = "/home/taavi/logs"
 
 keywords = "Windows", "Linux", "OS X", "Ubuntu", "Googlebot", "bingbot", "Android", "YandexBot", "facebookexternalhit"
 d = {}
 urls = {}
 users = {}
+ip_addresses = {}
+countries = {}
 total = 0
 usernr = 0
-import gzip
-import urllib
+
+gi = GeoIP.open("GeoIP.dat", GeoIP.GEOIP_MEMORY_CACHE)
  
 for filename in os.listdir(root):
     if not filename.startswith("access.log"):
@@ -27,7 +38,15 @@ for filename in os.listdir(root):
             source_timestamp, request, response, referrer, _, agent, _ = line.split("\"")
             method, path, protocol = request.split(" ")
             _, status_code, content_length, _ = response.split(" ")
-            content_length = int(content_length) 
+            content_length = int(content_length)
+            
+            source_ip, _, _, timestamp = source_timestamp.split(" ", 3)
+            
+            if not ":" in source_ip: #skip IPv6
+                ip_addresses[source_ip] = ip_addresses.get(source_ip,0) + 1
+                cc = gi.country_code_by_addr(source_ip)
+               # print source_ip, "resolve to", cc
+                countries[cc] = countries.get(cc, 0) + 1
             
             path = urllib.unquote(path)
             url="http://enos.itcollege.ee" + urllib.unquote(path)
@@ -38,6 +57,7 @@ for filename in os.listdir(root):
                     users[username] = users[username] + content_length
                 except:
                     users[username] = content_length
+                
             try:
                 urls[path] = urls[path] + 1
             except:
@@ -58,20 +78,50 @@ print "Devices:"
 results = d.items()
 results.sort(key = lambda item:item[1], reverse=True)
 for keyword, hits in results:
-    print keyword, "==>", hits, "(", hits * 100 / total, "%)"
+    print keyword, "==>", hits, "(", hits * 100 / total, "% of traffic)"
 
 print    
 print "Top 5 visited URL-s:"    
 results = urls.items()
 results.sort(key = lambda item:item[1], reverse=True)
 for path, hits in results [0:5]:
-    print path, "==>", hits, "(", hits * 100 / total, "%)"
+    print path, "==>", hits, "(", hits * 100 / total, "% of traffic)"
+    
+print    
+print "Top 5 IP-addresses:"    
+results = ip_addresses.items()
+results.sort(key = lambda item:item[1], reverse=True)
+for source_ip, hits in results [0:5]:
+    print source_ip, "==>", hits, " hits"
+    
+    
+print    
+print "Top 5 Countries:"    
+results = countries.items()
+results.sort(key = lambda item:item[1], reverse=True)
+for cc, hits in results [0:5]:
+    print cc, "==>", hits, " hits"
+
 
 print    
-print "Top 5 visited users-s:"    
+print "Top 5 bandwidth hoggers:"    
 results =  users.items()
 results.sort(key = lambda item:item[1], reverse=True)
 for username, hits in results [0:5]:
-    print username, "==>", hits / (1024*1024), "MB"
-    
-    
+    def humanize(bytes):
+        if bytes < 1024:
+            return  "%.2f B" % bytes
+        elif bytes < 1024 ** 2:
+            return "%.2f kB" % (bytes / 1024.0)
+        elif bytes < 1024 ** 3:
+            return "%.2f MB" % (bytes / 1024.0 ** 2)
+        elif bytes < 1024 ** 4:
+            return "%.2f GB" % (bytes / 1024.0 ** 3)
+        else:
+            return "%.2f TB" % (bytes / 1024.0 ** 4)
+
+
+        for filename in os.listdir("."):
+            mode, inode, devide, nlink, uid, gid, size, atime, mtime, ctime = os.stat(filename)
+   #         print filename, humanize(size)
+    print username, "==>", humanize(hits)
